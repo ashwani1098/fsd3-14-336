446a19749e6ce7211ea2636147000a4f45319316
Ashwani Kumar Singh <vikassingh95656435@gmail.com>
2026-09-10 14:23:36 +0530

    all prg's updated and lab-4

--- Lab-3/prg1.js
+++ Lab-3/prg1.js
@@ -1,15 +1,21 @@
-import http from "http"
+import http from "http";
 
-const server = http.createServer((req,res)=> {
-// jhkj
+const server = http.createServer((req, res) => {
+  console.log("Welcome to Node JS");
+  console.log(req.url);
+  console.log("Request Method:");
 
-    console.log("Welocme to the server")
-    console.log(req.url);
-    console.log("Request Method");
-    console.log(req.method);
-    console.log("Request Header");
-    console.log(req.headers);
-        res.end("hello")
+  console.log(req.method);
+
+  console.log("Request Header");
+  console.log(req.headers);
+  console.log("Socket Info");
+  //   console.log(req.socket);
+  console.log(req.headers.host);
+
+  res.end("hello");
 });
-const port = 5000
-server.listen(port,()=>console.log(`sever runnning http://localhost:${port}`));
+
+const PORT = 4444;
+
+server.listen(PORT, () => console.log("Server is running..."));
