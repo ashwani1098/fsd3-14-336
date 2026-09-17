5da91b06fec219f34cd663fbbacdbd9fe9c8b74e
Ashwani Kumar Singh <vikassingh95656435@gmail.com>
2026-09-17 14:02:23 +0530

    lab-5 start

---
+++ lab5/app.js
@@ -0,0 +1,22 @@
+import express from 'express'
+const app =express();
+
+app.get("/",(req,res)=>{
+    res.send("<h1> Hello Express");
+});
+app.get('/about',(req,res)=>{
+    res.send("<h2>We are FSD developers")
+});
+app.post('/login',(req,res)=>{
+    res.send({msg:'user login'})
+})
+app.put('/user/update/1',(req,res)=>{
+    res.send({msg:'user update'})
+})
+app.delete('/users/1',(req,res)=>{
+    res.send({msg:'remove user 1'})
+})
+app.use((req,res)=>{
+    res.status(404).send("Not Found")
+});
+app.listen(3333,()=> console.log("Server is running"));
